3e3f5287b8382ecc5ea0bb9ad5eae85014e782a5
Leonardo Fiório <leonardofiorio@id.uff.br>
2019-11-17 18:51:39 -0300

    Checking per minute

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import time, requests
 
+link = 'https://desafioperformance.b2w.io/bairros'
+
 # Status code lists 
 list_codes_2xx = [200,201,203,204,205,206,207,208,226]
 list_codes_4xx = [400,401,402,403,404,405,406,407,408,
@@ -14,41 +16,53 @@ qtt_2xx = 0
 qtt_4xx = 0
 qtt_5xx = 0
 
+
+
 if __name__ == '__main__':
 
-	while True:
-		print("GET Request...")
-		status_code = requests.get('https://desafioperformance.b2w.io').status_code
 
-		print("Status code: ")
-		print(status_code)
+	while True: 
+		
+		begin_time = time.time()
+		end_time = begin_time + 60
 
-		found = False
+		while time.time() < end_time:
+			print("GET Request...")
+			status_code = requests.get(link).status_code
 
-		i = 0
-		while( (not found) and i < len(list_codes_2xx)):
-			if status_code == list_codes_2xx[i]:
-				found = True
-				qtt_2xx = qtt_2xx + 1
-			i = i + 1
+			print("Status code: ")
+			print(status_code)
 
-		i = 0
-		while( (not found) and i < len(list_codes_4xx)):
-			if status_code == list_codes_4xx[i]:
-				found = True
-				qtt_4xx = qtt_4xx + 1 
-			i = i + 1
+			found = False
 
-		i = 0
-		while( (not found) and i < len(list_codes_5xx)):
-			if status_code == list_codes_5xx[i]:
-				found = True
-				qtt_5xx = qtt_5xx + 1
-			i = i + 1
+			i = 0
+			while( (not found) and i < len(list_codes_2xx)):
+				if status_code == list_codes_2xx[i]:
+					found = True
+					qtt_2xx = qtt_2xx + 1
+				i = i + 1
 
-		time.sleep(2)
 
-	# print("Quantity 2XX ", qtt_2xx)
-	# print("Quantity 4XX ", qtt_4xx)
-	# print("Quantity 5XX ", qtt_5xx)
-	# print("Encerrando...")
+			i = 0
+			while( (not found) and i < len(list_codes_4xx)):
+				if status_code == list_codes_4xx[i]:
+					found = True
+					qtt_4xx = qtt_4xx + 1 
+				i = i + 1
+
+
+			i = 0
+			while( (not found) and i < len(list_codes_5xx)):
+				if status_code == list_codes_5xx[i]:
+					found = True
+					qtt_5xx = qtt_5xx + 1
+				i = i + 1
+
+			time.sleep(2)
+
+
+			print("------------------------")
+			print("Quantity 2XX ", qtt_2xx)
+			print("Quantity 4XX ", qtt_4xx)
+			print("Quantity 5XX ", qtt_5xx)
+			print("Encerrando...")
